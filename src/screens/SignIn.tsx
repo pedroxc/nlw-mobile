f22e3f40de9dae93e154f26a246f47f5ae504db7
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-11-17 17:40:08 -0300

    build

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,21 +1,26 @@
 import { Center, Text, Icon } from "native-base";
+import { Image } from "react-native";
 import { Fontisto } from "@expo/vector-icons";
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
 
-import Logo from "../assets/logo.svg";
+import logo from "../assets/logo.svg";
 
 export function SignIn() {
   const { signIn, user } = useAuth();
 
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
-      <Logo width={212} height={40} />
+      <Image
+        source={logo}
+        style={{ tintColor: "white", width: 212, height: 40 }}
+      />
       <Button
         mt={12}
         onPress={signIn}
         type="SECONDARY"
         title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
       />
       <Text color="white" textAlign="center" mt={4}>
         Não utilizamos nenhuma informação além {"\n"}do seu e-mail para criação
