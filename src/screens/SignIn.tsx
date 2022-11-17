5a6344118049bc9079e78559651cf76fc1bd08be
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-11-17 17:32:47 -0300

    build

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -16,7 +16,6 @@ export function SignIn() {
         onPress={signIn}
         type="SECONDARY"
         title="ENTRAR COM GOOGLE"
-        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
       />
       <Text color="white" textAlign="center" mt={4}>
         Não utilizamos nenhuma informação além {"\n"}do seu e-mail para criação
